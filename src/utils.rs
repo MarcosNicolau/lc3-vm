f92290ec8d753aca712b6f3d57d652aa229891f0
Marcos Nicolau <marcosnicolau@lambdaclass.com>
2024-07-29 15:24:14 -0300

    disable input buffering

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use termios::*;
+
 pub fn read_file_as_u16(file_path: &str, buffer: &mut [u16], write_from: usize) {
     let contents = std::fs::read(file_path).unwrap();
 
@@ -15,4 +17,16 @@ pub fn sign_extend(mut x: u16, bit_count: u8) -> u16 {
     }
 
     x
+}
+
+pub fn disable_input_buffering() {
+    let mut termios: Termios = Termios::from_fd(0).unwrap();
+    termios.c_lflag &= !ICANON & !ECHO;
+    termios::tcsetattr(0, TCSANOW, &termios).unwrap();
+}
+
+pub fn restore_input_buffering() {
+    let mut termios = Termios::from_fd(0).unwrap();
+    termios.c_lflag |= ICANON | ECHO;
+    termios::tcsetattr(0, TCSANOW, &termios).unwrap();
 }
